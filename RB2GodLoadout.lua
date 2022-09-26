1987c8ca1afa58f27e690d81b05bb9a993411165
UserTac <105971360+UserTac@users.noreply.github.com>
2022-09-26 15:35:23 -0400

    Update RB2GodLoadout.lua

--- RB2GodLoadout.lua
+++ RB2GodLoadout.lua
@@ -1,1 +1,47 @@
-.
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("M4A1")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
+
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Remington 700")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
+
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Pump Shotgun")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
+
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Riot Shield")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
+
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Cuffs")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
+
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Frag Grenade")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
+
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer.Character,
+    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Incendiary Grenade")
+}
+game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
