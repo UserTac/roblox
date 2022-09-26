dceb749f5743d425cf2e65d6d389680d382a56e0
UserTac <105971360+UserTac@users.noreply.github.com>
2022-09-26 11:51:30 -0400

    Update RB2getfire.lua
    fixed?

--- RB2getfire.lua
+++ RB2getfire.lua
@@ -976,25 +976,3 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
-local args = {
-    [1] = game:GetService("Players").LocalPlayer.Character,
-    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Incendiary Grenade")
-}
-
-game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
-local args = {
-    [1] = game:GetService("Players").LocalPlayer.Character,
-    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Incendiary Grenade")
-}
-
-game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
-local args = {
-    [1] = game:GetService("Players").LocalPlayer.Character,
-    [2] = game:GetService("ReplicatedStorage").Assets.Loadout.Misc:FindFirstChild("Incendiary Grenade")
-}
-
-game:GetService("ReplicatedStorage").AddTool:FireServer(unpack(args))
---this
---was
---to 
---make 1000 lines
